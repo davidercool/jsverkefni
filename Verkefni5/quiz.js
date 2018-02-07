d9a46b31c1b9b1aa24632a7a62b987f230b24bac
davidercool <DBjarki@gmail.com>
2018-02-07 15:11:53 +0000

    byrjaði verkefni 5

---
+++ Verkefni5/quiz.js
@@ -0,0 +1,11 @@
+var question1 = {
+    question: "Who was the first man on the moon?",
+    answers: ["Buzz Alderaan","Buzz Lightyear","Buzz Aldrin"],
+    correctans: "Neil Armstrong"
+};
+
+
+var quiz = document.getElementById("quiz");
+
+
+spurning.textContent = question.questions[0];
