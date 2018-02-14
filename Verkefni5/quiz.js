002b8f1c9f02a90b3d6b7c63daad8eb581cc366e
davidercool <DBjarki@gmail.com>
2018-02-14 15:01:32 +0000

    shit

--- Verkefni5/quiz.js
+++ Verkefni5/quiz.js
@@ -1,11 +1,19 @@
 var question1 = {
     question: "Who was the first man on the moon?",
     answers: ["Buzz Alderaan","Buzz Lightyear","Buzz Aldrin"],
-    correctans: "Neil Armstrong"
+    correctAns: "Neil Armstrong"
 };
 
 
-var quiz = document.getElementById("quiz");
+var quiz = document.getElementsByClassName("hello");
+quizans  = quiz.getElementsByClassName("answers")[0];
+quizans1 = quiz.getElementsByClassName("answers")[1];
+quizans2 = quiz.getElementsByClassName("answers2")[0];
+quizans3 = quiz.getElementsByClassName("answers2")[1];
 
+quiz.innerHTML = question1.question;
 
-spurning.textContent = question.questions[0];
+quizans.innerHTML = question1.answers[1];
+quizans1.innerHTML = question1.correctAns;
+quizans2.innerHTML = question1.answers[0];
+quizans3.innerHTML = question1.answers[2];
