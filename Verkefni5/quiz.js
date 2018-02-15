c05ef83ce0332c824584ed451a4864ad22719916
David <dbjarki@gmail.com>
2018-02-15 20:57:26 +0000

    kláraði verkefni 5

--- Verkefni5/quiz.js
+++ Verkefni5/quiz.js
@@ -1,19 +1,14 @@
 var question1 = {
     question: "Who was the first man on the moon?",
-    answers: ["Buzz Alderaan","Buzz Lightyear","Buzz Aldrin"],
+    answers: ["Buzz Alderaan", "Buzz Lightyear", "Buzz Aldrin"],
     correctAns: "Neil Armstrong"
 };
 
 
-var quiz = document.getElementsByClassName("hello");
-quizans  = quiz.getElementsByClassName("answers")[0];
-quizans1 = quiz.getElementsByClassName("answers")[1];
-quizans2 = quiz.getElementsByClassName("answers2")[0];
-quizans3 = quiz.getElementsByClassName("answers2")[1];
+var quiz = document.getElementById("quiz");
 
-quiz.innerHTML = question1.question;
-
-quizans.innerHTML = question1.answers[1];
-quizans1.innerHTML = question1.correctAns;
-quizans2.innerHTML = question1.answers[0];
-quizans3.innerHTML = question1.answers[2];
+quiz.getElementsByClassName("hello")[0].innerHTML = question1.question;
+quiz.getElementsByClassName("w50")[0].innerHTML = question1.answers[2];
+quiz.getElementsByClassName("w50")[1].innerHTML = question1.answers[0];
+quiz.getElementsByClassName("w50")[2].innerHTML = question1.answers[1];
+quiz.getElementsByClassName("w50")[3].innerHTML = question1.correctAns;
