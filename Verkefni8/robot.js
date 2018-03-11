63829ede8b2967079b92d82fff611fd8179ace4d
davidercool <DBjarki@gmail.com>
2018-03-11 02:16:49 +0000

    Update robot.js

--- Verkefni8/robot.js
+++ Verkefni8/robot.js
@@ -35,92 +35,78 @@ class VillageState { // heldur utan um staðsetningar á vélmenni og delivery p
   move(destination) { // move function inni í klasanum
     if (!roadGraph[this.place].includes(destination)) { // ef að áfangastaður er ekki einu move frá þér
       return this; // skila gömlu staðsetningu
-    } else {
+    } else { // annars
       let parcels = this.parcels.map(p => {
-        if (p.place != this.place) return p;
-        return {place: destination, address: p.address};
-      }).filter(p => p.place != p.address);
-      return new VillageState(destination, parcels);
+        if (p.place != this.place) return p; // skilar parcels ef vélmennið er ekki á parcels
+        return {place: destination, address: p.address}; // skilar destination og parcel destination
+      }).filter(p => p.place != p.address); // hunsar allt sem er ekki parcel address
+      return new VillageState(destination, parcels); // Uppfærir VillageState
     }
   }
 }
-let first = new VillageState(
-  "Post Office",
-  [{place: "Post Office", address: "Alice's House"}]
-);
-let next = first.move("Alice's House");
 
-console.log(next.place);
-// → Alice's House
-console.log(next.parcels);
-// → []
-console.log(first.place);
-// → Post Office
-let object = Object.freeze({value: 5});
-object.value = 10;
-console.log(object.value);
-// → 5
-function runRobot(state, robot, memory) {
-  for (let turn = 0;; turn++) {
-    if (state.parcels.length == 0) {
-      console.log(`Done in ${turn} turns`);
-      break;
+function runRobot(state, robot, memory) { // function sem byrjar að hreyfa vélmennið
+  for (let turn = 0;; turn++) { // endalaust for loop sem heldur utan um hversu oft vélmennið hefur hreyft sig
+    if (state.parcels.length == 0) { // ef að öll parcels eru komin á réttann stað
+      console.log(`Done in ${turn} turns`); // prentar út hversu mörg turn það tók að klára allt
+      break; // stoppar for loopið
     }
-    let action = robot(state, memory);
-    state = state.move(action.direction);
-    memory = action.memory;
-    console.log(`Moved to ${action.direction}`);
+    let action = robot(state, memory); // action er það sem vélmennið gerir núverandi turn
+    state = state.move(action.direction); // uppfærir state
+    memory = action.memory; // uppfærir memory
+    console.log(`Moved to ${action.direction}`); // prentar út hvert vélmennið hreyfði sig
   }
-}
-function randomPick(array) {
-  let choice = Math.floor(Math.random() * array.length);
-  return array[choice];
 }
 
-function randomRobot(state) {
-  return {direction: randomPick(roadGraph[state.place])};
+function randomPick(array) { // velur random index úr array
+  let choice = Math.floor(Math.random() * array.length); // notar random functionið til að velja random index úr array
+  return array[choice]; // skilar handahófsvalda gildinu
 }
-VillageState.random = function(parcelCount = 5) {
-  let parcels = [];
-  for (let i = 0; i < parcelCount; i++) {
-    let address = randomPick(Object.keys(roadGraph));
-    let place;
-    do {
-      place = randomPick(Object.keys(roadGraph));
-    } while (place == address);
-    parcels.push({place, address});
+
+function randomRobot(state) { // vélmenni sem hreyfir sig randomly
+  return {direction: randomPick(roadGraph[state.place])}; // skilar random átt til að fara í
+}
+
+VillageState.random = function(parcelCount = 5) { // býr til random village
+  let parcels = []; // býr til tómt array fyrir parcels
+  for (let i = 0; i < parcelCount; i++) { // for loop sem býr tl parcels
+    let address = randomPick(Object.keys(roadGraph)); // setur random parcel addressu
+    let place; // staðsetning
+    do { // do while loop
+      place = randomPick(Object.keys(roadGraph)); // velur random staðsetningu sem place
+    } while (place == address); // á meðan place == address
+    parcels.push({place, address}); // pushar place og address í parcels arrayið
   }
-  return new VillageState("Post Office", parcels);
+  return new VillageState("Post Office", parcels); //skilar VillageState með öllum parcel staðsetningum
 };
-runRobot(VillageState.random(), randomRobot);
-// → Moved to Marketplace
-// → Moved to Town Hall
-// → …
-// → Done in 63 turns
+
 const mailRoute = [
   "Alice's House", "Cabin", "Alice's House", "Bob's House",
   "Town Hall", "Daria's House", "Ernie's House",
   "Grete's House", "Shop", "Grete's House", "Farm",
   "Marketplace", "Post Office"
-];
-function routeRobot(state, memory) {
-  if (memory.length == 0) {
-    memory = mailRoute;
+]; // Route fyrir routeRobot
+
+function routeRobot(state, memory) { // vélmenni sem notar mailRoute til að færa sig á milli staða
+  if (memory.length == 0) { // ef memory er tómt
+    memory = mailRoute; // breyta memory í mailroute
   }
-  return {direction: memory[0], memory: memory.slice(1)};
+  return {direction: memory[0], memory: memory.slice(1)}; // skilar áttinni sem hann á að fara í
 }
-function findRoute(graph, from, to) {
-  let work = [{at: from, route: []}];
-  for (let i = 0; i < work.length; i++) {
-    let {at, route} = work[i];
+
+function findRoute(graph, from, to) { // finnur stystu leið að næsta parcel
+  let work = [{at: from, route: []}]; // array sem heldur utan um at og route
+  for (let i = 0; i < work.length; i++) { // for loop sem keyrir jafn oft og lengdin á work arrayinu
+    let {at, route} = work[i]; // 
     for (let place of graph[at]) {
-      if (place == to) return route.concat(place);
-      if (!work.some(w => w.at == place)) {
-        work.push({at: place, route: route.concat(place)});
+      if (place == to) return route.concat(place); // ef place == to 
+      if (!work.some(w => w.at == place)) { // 
+        work.push({at: place, route: route.concat(place)}); // 
       }
     }
   }
 }
+
 function goalOrientedRobot({place, parcels}, route) {
   if (route.length == 0) {
     let parcel = parcels[0];
@@ -132,3 +118,4 @@ function goalOrientedRobot({place, parcels}, route) {
   }
   return {direction: route[0], memory: route.slice(1)};
 }
+runRobot(VillageState.random(), randomRobot);
