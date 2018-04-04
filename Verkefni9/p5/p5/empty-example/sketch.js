d107028cb75297c88c71f5116ff505320f45e898
davidercool <DBjarki@gmail.com>
2018-04-04 22:14:02 +0000

    basic functions tilbúin
    p5 komið

--- Verkefni9/p5/p5/empty-example/sketch.js
+++ Verkefni9/p5/p5/empty-example/sketch.js
@@ -3,9 +3,9 @@ var enemy = [];
 var bullet = [];
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(600, 400);
     ship = new Ship();
-    bullet = new Bullet(width/2, height/2);
+    //bullet2 = new Bullet(width/2, height/2);
     for (var i = 0; i < 6; i++) {
         enemy[i] = new Enemy(i*55+30, 40);
     }
@@ -14,14 +14,23 @@ function setup() {
 function draw() {
     background(51);
     ship.show();
-    bullet.show();
-    bullet.move();
+
+    
+    for (var i = 0; i < bullet.length; i++) {
+        bullet[i].show();
+        bullet[i].move();
+    }
+    
     for (var i = 0; i < enemy.length; i++) {
         enemy[i].show();
     }
 }
 
 function keyPressed() {
+    if (key === ' ') {
+        var bullets = new Bullet(ship.x, ship.y);
+        bullet.push(bullets);
+    }
     if (keyCode === RIGHT_ARROW) {
         ship.move(1);
     } else if (keyCode === LEFT_ARROW) {
